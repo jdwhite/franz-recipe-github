c04a9cb114d3a961dd6c7edab796542d835fb823
Jason White <jdwhite@menelos.com>
2019-09-01 12:45:47 -0500

    Explicitly set direct and indrect counts in setBadge().

--- webview.js
+++ webview.js
@@ -2,17 +2,20 @@
 
 module.exports = Franz => {
   const getMessages = () => {
-    var element = document.querySelector('[aria-label="You have unread notifications"]');
-    var hasUnread = 0;
-	//console.log(element);
+    //var element = document.querySelector('[aria-label="You have unread notifications"]');
+    var element = document.querySelector('span[class="mail-status unread"]');
+    const direct = 0;
+    var indirect = 0;
+    console.log("element="+element);
 
     if (element !== null) {
-      hasUnread = 1;
+      console.log("element !== null; indirect should be 1");
+      indirect = 1;
     }
+    console.log("indirect="+indirect);
 
-    Franz.setBadge(0, hasUnread);
+    Franz.setBadge(direct,indirect);
   };
 
   Franz.loop(getMessages);
-
 };
